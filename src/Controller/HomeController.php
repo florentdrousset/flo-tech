cd7c99b485df930bc84996ca0b08159770bdb26f
Florent Drousset <florent.drousset@gmail.com>
2023-04-22 20:00:45 +0200

    add bulma and first layout tests

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/')]
+public function home(Request $request): \Symfony\Component\HttpFoundation\Response
+    {
+        return $this->render('home/home.html.twig', [
+        ]);    }
+}
