b98cc81459d0c85c993aa73a3dada5e8d96d6db3
Florent Drousset <florent.drousset@gmail.com>
2023-05-01 23:02:11 +0200

    more design fixes

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,12 +5,24 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ArticleRepository;
+
+
 
 class HomeController extends AbstractController
 {
+    private ArticleRepository $articleRepository;
+
+    public function __construct(ArticleRepository $articleRepository) {
+        $this->articleRepository = $articleRepository;
+    }
+    
     #[Route('/')]
 public function home(Request $request): \Symfony\Component\HttpFoundation\Response
     {
+        $articles = $this->articleRepository->findAll();
+
         return $this->render('home/home.html.twig', [
+            'articles' => $articles
         ]);    }
     }
